770a5912bf7816ffbabb4277cdc2f44a32d172d8
iamfurkann <esadfurkanduman@gmail.com>
2025-04-29 16:31:55 +0300

    aaaaaaaaaaaaaaaaaaaaaaaaaaaaaa

---
+++ C08/ex00/ft.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft.h                                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: esduman <esduman@student.42istanbul.com.t  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/29 16:22:11 by esduman           #+#    #+#             */
+/*   Updated: 2025/04/29 16:26:43 by esduman          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_H
+# define FT_H
+
+void	ft_putchar(char c);
+void	ft_swap(int *a, int *b);
+void	ft_putstr(char *str);
+int		ft_strlen(char *str);
+int		ft_strcmp(char *s1, char *s2);
+#endif
